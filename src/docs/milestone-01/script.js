17df9ad382da0584e009f81227f47fae6663592f
Kevin Li <kevinlouisli7@gmail.com>
2024-04-02 23:18:05 -0400

    Finalized milestone01

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -2,10 +2,18 @@ const UPDATE_PER_SECOND = 10;
 const CO2_PER_UPDATE = 1337.724 / UPDATE_PER_SECOND;
 let updateCount = 0;
 
-const carbon = document.getElementById("carbon");
+const textBox = document.getElementById("carbonEmissions");
+const button = document.getElementById("carbonButton");
 
-setInterval(() => {
-  carbon.innerText = (
-    Math.round(++updateCount * CO2_PER_UPDATE * 100) / 100
-  ).toFixed(2);
-}, 1000 / UPDATE_PER_SECOND);
+button.addEventListener("click", function () {
+  button.remove();
+  textBox.innerHTML = `Since you've clicked the button,
+  <strong><span id="carbon">0</span></strong> tons of CO2 have been
+  released into the atmosphere.`;
+  const carbon = document.getElementById("carbon");
+  setInterval(() => {
+    carbon.innerText = (
+      Math.round(++updateCount * CO2_PER_UPDATE * 100) / 100
+    ).toFixed(2);
+  }, 1000 / UPDATE_PER_SECOND);
+});
