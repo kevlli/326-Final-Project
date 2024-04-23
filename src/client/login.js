d2698f461324bd8ef6c04e605740da40aaced06a
Kevin Li <kevinlouisli7@gmail.com>
2024-04-23 17:10:17 -0400

    Added milestone-02 script to npm, added draft for login page

---
+++ src/client/login.js
@@ -0,0 +1,21 @@
+const loginForm = document.getElementById("loginForm");
+const signupForm = document.getElementById("signupForm");
+const container = document.getElementById("container");
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("loginUsername").value;
+  const password = document.getElementById("loginPassword").value;
+  // send data for authentication, to be implemented
+  console.log("Login submitted:", { username, password });
+  container.innerHTML = `Successfully Logged In! 
+        <a href="home.html">Home</a>`;
+});
+
+signupForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("signupUsername").value;
+  const password = document.getElementById("signupPassword").value;
+  // send data to authentication, to be implemented
+  console.log("Sign Up submitted:", { username, password });
+});
